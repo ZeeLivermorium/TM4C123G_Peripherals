f2d809bae1f8b84ea7e7b2b935dd7b308373dac2
ZeeLivermorium <zeelivermorium@gmail.com>
2017-12-24 17:01:35 -0600

    update SSI0 and organized PWM folder

--- SSI(SPI)/SSI0(PA2-5)/SSI0.c
+++ SSI(SPI)/SSI0(PA2-5)/SSI0.c
@@ -20,7 +20,7 @@
  * Discription: Initialize SSI0(PA2-5).
  */
 void SSI0_Init(void) {
-    /* */
+    /* SSI and Port A Activation */
     SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;                 // enable SSI Module 0 clock
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;               // enable GPIO Port A clock
     while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R0) == 0){};    // allow time for activating
@@ -39,12 +39,28 @@ void SSI0_Init(void) {
     GPIO_PORTA_DEN_R |= 0x2C;                              // enable digital I/O on PA2-5
     
     /* SSI0 Set Up */
-    SSI0_CR1_R = 0x0;                                      //
-    SSI0_CPSR_R = 0x02;                                    //
-    SSI0_CR0_R &= ~(0x0000FFF0);                           //
-    SSI0_CR0_R |= 0x0F;                                    //
-    SSI0_DR_R = initialData;                               //
-    SSI0_CR1_R |= 0x02;                                    //
+    SSI0_CR1_R &= ~SSI_CR1_SSE;                            // disable SSI operation
+    SSI0_CR1_R &= ~SSI_CR1_MS;                             // configure SSI0 as master mode
+    SSI0_CPSR_R &= SSI_CPSR_CPSDVSR_M;                     // clear bit fields for SSI Clock Prescale Divisor
+    SSI0_CPSR_R += 2;                                      // /2 clock divisor, must be even number in [0, 254]
+    SSI0_CR0_R &= ~SSI_CR0_SCR_M;                          // clear bit fields for SSI0 Serial Clock Rate, SCR = 0
+    SSI0_CR0_R &= ~SSI_CR0_SPH;                            // clear bit fields for SSI0 Serial Clock Phase, SPH = 0
+    SSI0_CR0_R &= ~SSI_CR0_SPO;                            // clear bit fields for SSI Serial Clock Polarity, SPO = 0
+    SSI0_CR0_R &= ~SSI_CR0_FRF_M;                          // clear bit fields for SSI0 Frame Format Select
+    SSI0_CR0_R |= SSI_CR0_FRF_MOTO;                        // set frame format to Freescale SPI Frame Format
+    SSI0_CR0_R &= ~SSI_CR0_DSS_M;                          // clear bit fields for SSI0 Data Size Select
+    SSI0_CR0_R |= SSI_CR0_DSS_16;                          // set SSI data size to 16
+    SSI0_CR1_R |= SSI_CR1_SSE;                             // enable SSI operation
 }
 
+
+uint16_t SSI0_Read(void) {
+    
+    return SSI0_DR_R;
+}
+
+void SSI0_Write(uint16_t data) {
+    while((SSI0_SR_R&SSI_SR_TNF)==0){};                    // wait until transmit FIFO not full
+    SSI0_DR_R = data;                                      // write data
+}
 
